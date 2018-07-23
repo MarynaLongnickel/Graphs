61cbf1d187298d525fcbdf3f6c199daf8cc999d3
MarynaLongnickel <37321032+MarynaLongnickel@users.noreply.github.com>
2018-07-23 15:54:45 -0400

    Update graph.py

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,11 +1,20 @@
-#!/usr/bin/python
-
-"""
-Simple graph implementation compatible with BokehGraph class.
-"""
-
-
 class Graph:
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, data):
+        if data in self.vertices.keys():
+            raise Exception('Vertex %s already exists' % data)
+        else:
+            self.vertices[data] = set([])
+
+    def add_edge(self, v1, v2):
+        if v1 not in self.vertices.keys():
+            raise Exception('Vertex %s does not exist' % v1)
+        elif v2 not in self.vertices.keys():
+            raise Exception('Vertex %s does not exist' % v2)
+        elif v1 in self.vertices[v2]:
+            raise Exception('Edge already exists')
+        else:
+            self.vertices[v1].add(v2)
+            self.vertices[v2].add(v1)
